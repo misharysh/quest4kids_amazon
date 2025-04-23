9465df20099ee4dd0d5f4f776223311590bf1f07
Mysha <mykhailo.ryshchenko@gentlecode.com>
2025-04-23 12:09:10 +0300

    - Added creation of 'task_status_enum' to initial migration;

--- src/migrations/1740136110266-InitialMigration.ts
+++ src/migrations/1740136110266-InitialMigration.ts
@@ -5,6 +5,10 @@ export class InitialMigration1740136110266 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `CREATE TYPE "public"."task_status_enum" AS ENUM('OPEN', 'IN_PROGRESS', 'DONE')`
+    );
+
+    await queryRunner.query(
       `CREATE TABLE "task" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying(100) NOT NULL, "description" text NOT NULL, "status" "public"."task_status_enum" NOT NULL DEFAULT 'OPEN', CONSTRAINT "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id"))`,
     );
   }
