6a116c6118aadc8ae070de25d2aac37ce9e59802
Mysha <mykhailo.ryshchenko@gentlecode.com>
2025-06-05 18:09:27 +0300

    Added TLS connection to redis;

--- src/config/bull.config.ts
+++ src/config/bull.config.ts
@@ -4,9 +4,8 @@ dotenv.config();
 
 export const redisConfig: QueueOptions = {
     connection: {
-        // host: process.env.REDIS_HOST || 'localhost',
-        // port: parseInt(process.env.REDIS_PORT ?? '6379', 10)
-        host: 'q4kredis-x5l5ko.serverless.use1.cache.amazonaws.com',
-        port: 6379
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
+        tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
     },
 };
