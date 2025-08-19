823213e0e1b6194c200db9b9608a6f9f57956c01
Mysha <mykhailo.ryshchenko@gentlecode.com>
2025-08-19 16:26:35 +0300

    Implementing custom logging to console and database;

---
+++ src/logging/log-entry.ts
@@ -0,0 +1,9 @@
+import { LogLevel } from "./log-level.enum";
+
+export class LogEntry {
+    timestamp: string;
+    level: LogLevel;
+    category: string;
+    message: string;
+    properties?: object;
+}
---
+++ src/logging/loggers/console-logging.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Scope } from "@nestjs/common";
+import { LogEntry } from "../log-entry";
+import { LogLevel } from "../log-level.enum";
+import { ILoggingService } from "../logging.interfaces";
+
+@Injectable({scope: Scope.REQUEST})
+export class ConsoleLoggingService implements ILoggingService
+{
+    private context: Record<string, any> = {};
+    private category = '';
+
+    setCategory(category: string) {
+        this.category = category;
+    }
+
+    scope(properties: Record<string, any>): void {
+        this.context = {...this.context, ...properties};
+    }
+
+    log(level: LogLevel, message: string, properties?: object): void {
+        const entry: LogEntry = 
+        {
+            timestamp: new Date().toISOString(),
+            level,
+            category: this.category,
+            message,
+            properties: {
+            ...this.context,
+            ...(properties || {}),
+            },
+        };
+
+        console.log(JSON.stringify(entry));
+    }
+}
